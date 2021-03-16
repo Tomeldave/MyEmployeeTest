51898e90dd6f9cf852a6d7f8287193367f910ef5
Tomeldave <78346047+Tomeldave@users.noreply.github.com>
2021-03-16 18:21:52 +0100

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employimg_Test
+{
+    class Employee
+    {
+       private string firstName;
+       private string lastName;
+       private decimal monthlySalary;
+       private decimal salaryIncrease;
+
+        public Employee(string first, string last, decimal salary, decimal increase)
+        {
+
+            firstName = first;
+            lastName = last;
+            monthlySalary = salary;
+            salaryIncrease = increase;
+
+         }
+
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+
+        }
+
+        public string LastName
+        {
+            get
+         
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+
+        }
+
+
+        public decimal MonthlySalary
+        {
+            get
+            {
+                return monthlySalary;
+
+            }
+            set
+            {
+                if (value >= 0)
+                    monthlySalary = value;
+                else
+                    monthlySalary = 0;
+
+            }
+        }
+        
+
+         public decimal SalaryIncrease
+         {
+            get
+
+            {
+                return salaryIncrease;
+            }
+            set
+            {
+                salaryIncrease = value;
+            }
+
+        }
+
+
+       
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employimg_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee( "Oladayo", "John", 3000.00M, 36000.00M);
+            
+            Employee employee2 = new Employee( "Tomiseen", "Emmanuel", 2000.00M, 24000.00M);
+
+            decimal yearlySalary1 = 3000.00M * 12;
+            decimal yearlySalary2 = 2000.00M * 12;
+            decimal salaryIncrease1 = yearlySalary1 + (yearlySalary1 * 0.1M);
+            decimal salaryIncrease2 = yearlySalary2 + (yearlySalary2 * 0.1M);
+
+            Console.WriteLine("Employee1 = Oladayo ");
+            Console.WriteLine();
+            Console.WriteLine("Oladayo's yearly salary is = " + yearlySalary1);
+            Console.WriteLine();
+            Console.WriteLine("Employee2 = Tomiseen");
+            Console.WriteLine();
+            Console.WriteLine("Tomiseen's yearly salary is = " + yearlySalary2);
+            Console.WriteLine();
+            Console.WriteLine("Employee1 = Oladayo ");
+            Console.WriteLine();
+            Console.WriteLine("Oladayo's salary increase is = " + salaryIncrease1);
+            Console.WriteLine();
+            Console.WriteLine("Employee2 = Tomiseen ");
+            Console.WriteLine();
+            Console.WriteLine("Tomiseen's salary increase is = " + salaryIncrease2);
+
+
+
+
+          Console.ReadLine();
+        }
+
+    
+    }
+
+}
